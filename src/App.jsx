938439d9a4ce502abb0dc38b274fb9232c01fae8
Sergey <sergsmile29@gmail.com>
2023-10-24 23:14:02 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import React from 'react'
-import { Employee } from './components/Employees/Employee'
-import { RegisterForm } from './components/RegisterForm/RegisterForm'
-import { Counter } from './components/Counter/Counter'
-import { TodoList } from './components/TodoList/TodoList'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import colorsData from './assets/colors.json'
-import { FirstScreen } from './components/FirstScreen/FirstScreen'
+import Posts from './components/Posts/Posts'
+
 class App extends React.Component {
-	state = {
-		isOpenFirstScreen: true,
-	}
-	componentDidMount() {
-		setTimeout(() => {
-			this.setState({ isOpenFirstScreen: false })
-		}, 4000)
-	}
 	render() {
 		return (
 			<div>
-				<Employee />
-				{/* <RegisterForm /> */}
-				<Counter />
-				{/* <TodoList /> */}
-				<ColorPicker colors={colorsData} />
-				{this.state.isOpenFirstScreen ? <FirstScreen /> : null}
+				<Posts />
 			</div>
 		)
 	}
