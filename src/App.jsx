16960e821fdc3bc8e8ddc1d8667fae385d699084
Sergey <sergsmile29@gmail.com>
2023-10-30 23:40:54 +0200

    create m5l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
-import React, { useContext } from 'react'
-import { Posts } from './components/Posts/Posts'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
 import colors from './assets/colors.json'
-import { Counter } from './components/Counter/Counter'
-import { Employee } from './components/Employees/Employee'
-import { TodoList } from './components/TodoList/TodoList'
-import { MyContext } from './context/ContextProvider'
-import { LoginForm } from './components/LoginForm/LoginForm'
 const App = () => {
-	const { isLoggedIn, logout } = useContext(MyContext)
-	// Якщо не залогована
-	if (!isLoggedIn) {
-		return <LoginForm />
-	}
-	return (
-		<div>
-			<button onClick={logout}>Exit</button>
-			{/* <Counter /> */}
-			{/* <ColorPicker colors={colors} /> */}
-			<Posts />
-			{/* <Employee auto={this.props.auto} /> */}
-			{/* <TodoList /> */}
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { GlobalStyles } from './styles/Global'
-import { ContextProvider } from './context/ContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<ContextProvider>
+	<>
 		<App auto='lada 2101' />
 		<ToastContainer autoClose={1500} />
-	</ContextProvider>
+	</>
 )
