65ba9e6a502b550414ee8f4fb98a9ed4470a029a
Sergey <sergsmile29@gmail.com>
2023-11-06 10:36:29 +0200

    builder callback

--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -7,30 +7,48 @@ const initialState = {
 	counter: 1,
 }
 
-// createReducer object notation
-export const counterReducer = createReducer(initialState, {
-	// [increment]: (state, action) => {
-	// 	return {
-	// 		...state,
-	// 		counter: state.counter + state.step,
-	// 	}
-	// },
-	[increment]: (state, action) => {
-		state.counter += state.step
-	},
-	[decrement]: (state, action) => {
-		if (state.counter > -2) {
+// builder callback
+export const counterReducer = createReducer(initialState, builder => {
+	builder
+		.addCase(increment, (state, action) => {
+			state.counter += state.step
+		})
+		.addCase(decrement, (state, action) => {
 			state.counter -= state.step
-		}
-	},
-	[reset]: (state, action) => {
-		state.counter = 0
-		state.step = 1
-	},
-	[changeStep]: (state, { payload }) => {
-		state.step = payload
-	},
+		})
+		.addCase(reset, (state, action) => {
+			state.counter = 1
+			state.step = 1
+		})
+		.addCase(changeStep, (state, { payload }) => {
+			state.step = payload
+		})
 })
+
+// createReducer object notation
+// export const counterReducer = createReducer(initialState, {
+// 	// [increment]: (state, action) => {
+// 	// 	return {
+// 	// 		...state,
+// 	// 		counter: state.counter + state.step,
+// 	// 	}
+// 	// },
+// 	[increment]: (state, action) => {
+// 		state.counter += state.step
+// 	},
+// 	[decrement]: (state, action) => {
+// 		if (state.counter > -2) {
+// 			state.counter -= state.step
+// 		}
+// 	},
+// 	[reset]: (state, action) => {
+// 		state.counter = 0
+// 		state.step = 1
+// 	},
+// 	[changeStep]: (state, { payload }) => {
+// 		state.step = payload
+// 	},
+// })
 
 // export const counterReducer = (state = initialState, action) => {
 // 	switch (action.type) {
