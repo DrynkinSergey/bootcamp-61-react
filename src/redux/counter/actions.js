fa3ded0f4814ad9cfaa6eb198e6ea3aa774ad38a
Sergey <sergsmile29@gmail.com>
2023-11-06 10:28:05 +0200

    object notation reducer

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,7 +1,19 @@
-import { CHANGE_INPUT, MINUS, PLUS, RESET } from './constants'
+import { createAction } from '@reduxjs/toolkit'
+// import { CHANGE_INPUT, MINUS, PLUS, RESET } from './constants'
 
 // Action Creators
-export const increment = () => ({ type: PLUS })
-export const decrement = () => ({ type: MINUS })
-export const reset = () => ({ type: RESET })
-export const changeStep = step => ({ type: CHANGE_INPUT, payload: step })
+// export const increment = () => ({ type: PLUS })
+// export const decrement = () => ({ type: MINUS })
+// export const reset = () => ({ type: RESET })
+// export const changeStep = step => ({ type: CHANGE_INPUT, payload: step })
+
+export const increment = createAction('plus')
+export const decrement = createAction('minus')
+export const reset = createAction('reset')
+export const changeStep = createAction('changeStep')
+
+// console.log(increment())
+// console.log(increment(21))
+// console.log(increment('test payload'))
+// console.log(increment({ user: { name: 'Alex' } }))
+// console.log(increment(['hello']))
--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,3 +1,5 @@
+import { createReducer } from '@reduxjs/toolkit'
+import { changeStep, decrement, increment, reset } from './actions'
 import { CHANGE_INPUT, MINUS, PLUS, RESET } from './constants'
 
 const initialState = {
@@ -5,24 +7,49 @@ const initialState = {
 	counter: 1,
 }
 
-export const counterReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case PLUS: {
-			return { ...state, counter: state.counter + state.step }
+// createReducer object notation
+export const counterReducer = createReducer(initialState, {
+	// [increment]: (state, action) => {
+	// 	return {
+	// 		...state,
+	// 		counter: state.counter + state.step,
+	// 	}
+	// },
+	[increment]: (state, action) => {
+		state.counter += state.step
+	},
+	[decrement]: (state, action) => {
+		if (state.counter > -2) {
+			state.counter -= state.step
 		}
-		case MINUS: {
-			return { ...state, counter: state.counter - state.step }
-		}
-		case RESET: {
-			return { ...initialState }
-		}
-		case CHANGE_INPUT: {
-			return {
-				...state,
-				step: action.payload,
-			}
-		}
-		default:
-			return state
-	}
-}
+	},
+	[reset]: (state, action) => {
+		state.counter = 0
+		state.step = 1
+	},
+	[changeStep]: (state, { payload }) => {
+		state.step = payload
+	},
+})
+
+// export const counterReducer = (state = initialState, action) => {
+// 	switch (action.type) {
+// 		case increment.type: {
+// 			return { ...state, counter: state.counter + state.step }
+// 		}
+// 		case decrement.type: {
+// 			return { ...state, counter: state.counter - state.step }
+// 		}
+// 		case reset.type: {
+// 			return { ...initialState }
+// 		}
+// 		case changeStep.type: {
+// 			return {
+// 				...state,
+// 				step: action.payload,
+// 			}
+// 		}
+// 		default:
+// 			return state
+// 	}
+// }
